6ee517fc14b09d1939f0db1644c3613a5dec3855
unknown <zhang.zhi@nationz.com.cn>
2017-07-25 10:43:39 +0800

    Chueng Zi

--- source/GenPassword.py
+++ source/GenPassword.py
@@ -12,15 +12,16 @@ Created on Fri Jul 21 15:04:54 2017
 import random, string, time
 def GenPasswordStandard(length):
     #随机出数字的个数
-    numOfNum = random.randint(1,length-1)
-    numOfLetter = length - numOfNum
+    numOfNum = random.randint(1,length-2)
+    numOfUpLetter = random.randint(1,length - numOfNum - 1)
+    numOfLowLetter = length - numOfNum - numOfUpLetter
     #选中numOfNum个数字
     slcNum = [random.choice(string.digits) for i in range(numOfNum)]
+    slcUpLetter = [random.choice(string.ascii_uppercase) for i in range(numOfUpLetter)]
+    slcLowLetter = [random.choice(string.ascii_lowercase) for i in range(numOfLowLetter)]
     #选中numOfLetter个字母
-    slcLetter = [random.choice(string.ascii_letters) for i in range(numOfLetter)]
-    #slcLetter = [random.choice(string.printable) for i in range(numOfLetter)]
     #打乱这个组合    
-    slcChar = slcNum + slcLetter
+    slcChar = slcNum + slcUpLetter + slcLowLetter
     random.shuffle(slcChar)
     #生成密码
     genPwd = ''.join([i for i in slcChar])
@@ -34,10 +35,24 @@ def GenPasswordSimple(length):
     return genPwd
 
 def GenPasswordComplex(length):
+     #随机出数字的个数
+    numOfNum = random.randint(1,length-3)
+    numOfUpLetter = random.randint(1,length - numOfNum - 2)
+    numOfLowLetter = random.randint(1,length - numOfNum - numOfUpLetter- 1)
+    numOfSymbol = length - numOfNum - numOfUpLetter - numOfLowLetter 
+    #选中numOfNum个数字
+    slcNum = [random.choice(string.digits) for i in range(numOfNum)]
+    #选中numOfLetter个字母
+    slcUpLetter = [random.choice(string.ascii_uppercase) for i in range(numOfUpLetter)]
+    slcLowLetter = [random.choice(string.ascii_lowercase) for i in range(numOfLowLetter)]
+    slcSymbol = [random.choice(string.punctuation) for i in range(numOfSymbol)]
+    #slcLetter = [random.choice(string.printable) for i in range(numOfLetter)]
+    #打乱这个组合    
     #随机出数字的个数
-    slcPrint = [random.choice(string.printable) for i in range(length)]
+    slcChar = slcNum + slcUpLetter + slcLowLetter + slcSymbol
+    random.shuffle(slcChar)
     #生成密码
-    genPwd = ''.join([i for i in slcPrint])
+    genPwd = ''.join([i for i in slcChar])
     return genPwd
 
 
@@ -48,7 +63,7 @@ if __name__ == '__main__':
     fp.writelines('Seed '+str(Seed)+'\n')
     random.seed(Seed)
     #random.seed(1)
-    fp.writelines(GenPasswordSimple(6)+'\n')
-    fp.writelines(GenPasswordStandard(6)+'\n')
-    fp.writelines(GenPasswordComplex(6)+'\n')
+    fp.writelines(GenPasswordSimple(8)+'\n')
+    fp.writelines(GenPasswordStandard(8)+'\n')
+    fp.writelines(GenPasswordComplex(8)+'\n')
     fp.close
