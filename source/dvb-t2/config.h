986cd287013271c935e56b7ab8e8ff449892e520
unknown <zhang.zhi@nationz.com.cn>
2017-09-20 18:09:35 +0800

    signoff on 2017-09-20

---
+++ source/dvb-t2/config.h
@@ -0,0 +1,20 @@
+/*
+  ============================================================================
+  Name        : Constellation.c
+  Author      : CC
+  Version     :
+  Copyright   : NZ
+  Description : Hello World in C, Ansi-style
+  ============================================================================
+*/
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+typedef struct __cfg
+{
+  char mod; // constellation type, bpsk, qpsk, 16-qam, 64-qam
+  int lenth; // data lenth in bit.
+}cfg_t;
+
+#endif
---
+++ source/dvb-t2/main.c
@@ -0,0 +1,23 @@
+/*
+ ============================================================================
+ Name        : Constellation.c
+ Author      : CC
+ Version     :
+ Copyright   : NZ
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "config.h"
+#include "tx_bmap_wr.h"
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+
+  //init_cfg(config);
+  //tx_bmap_wr(config, FidLogFile);
+
+	return EXIT_SUCCESS;
+}
