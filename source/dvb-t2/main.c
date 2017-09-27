629830d908495e277940d44c25d4e27c57753d6a
unknown <zhang.zhi@nationz.com.cn>
2017-09-27 18:06:49 +0800

    Chueng Zi

--- source/dvb-t2/main.c
+++ source/dvb-t2/main.c
@@ -17,6 +17,7 @@
 #include "tx_bmap_wr.h"
 #include "tx_bmap_di_gen.h"
 #include "rx_bmap_wr.h"
+#include "cmp_tx_rx.h"
 
 int main(void) {
 
@@ -24,7 +25,7 @@ int main(void) {
   //FILE *FidLogFile;
    FILE *FidCfgFile;
 
-  int *TxDI;
+  int *TxDi;
   float *TxDoI;
   float *TxDoQ;
 
@@ -44,15 +45,18 @@ int main(void) {
 
   if (config->InSrc == 0)
   {
-    tx_bmap_di_gen(config->numBits, &TxDI);
+    tx_bmap_di_gen(config->numBits, &TxDi);
   }
 
-  tx_bmap_wr(config, &TxDI, &TxDoI, &TxDoQ);
+  tx_bmap_wr(config, &TxDi, &TxDoI, &TxDoQ);
 
   RxDiI = TxDoI;
   RxDiQ = TxDoQ;
 
   rx_bmap_wr(config, &RxDiI, &RxDiQ, &RxDo);
+
+
+  cmp_tx_rx(config->numBits, &TxDi, &RxDo);
 
   fclose(FidCfgFile);
   //fclose(FidLogFile);
--- source/dvb-t2/rx_bmap_mux.c
+++ source/dvb-t2/rx_bmap_mux.c
@@ -12,5 +12,71 @@
 #include "debug.h"
 void rx_bmap_mux(int Mod, int numBits, int **DO, int **DI)
 {
-  ;
+  unsigned InLen;
+	unsigned OutLen;
+
+	int i;
+
+	OutLen = numBits;
+	switch (Mod){
+	case 0 :
+		InLen = OutLen;
+		break;
+	case 1 :
+		InLen = OutLen / 2;
+		break;
+	case 2 :
+		InLen = OutLen / 4;
+		break;
+	case 3 :
+		InLen = OutLen / 6;
+		break;
+	default :
+		assert(Mod<=3);
+    InLen = 0;
+	}
+
+	*DO = (int *)malloc(numBits * sizeof(int));
+  
+  switch (Mod){
+
+	case 0 :
+		for(i = 0; i < InLen; i++){
+			(*DO)[i] = (*DI)[i];
+		}
+		break;
+    
+	case 1 :
+		for(i = 0; i < InLen; i++){
+			(*DO)[i*2]   = ((*DI)[i] & 0x1) ;
+			(*DO)[i*2+1] = ((*DI)[i] & 0x2) >>1 ;
+		}
+		break;
+
+	case 2 :
+		for(i = 0; i < InLen; i++){
+			(*DO)[i*4]   = ((*DI)[i] & 0x1) ;
+			(*DO)[i*4+1] = ((*DI)[i] & 0x2) >> 1 ;
+			(*DO)[i*4+2] = ((*DI)[i] & 0x4) >> 2 ;
+			(*DO)[i*4+3] = ((*DI)[i] & 0x8) >> 3 ;
+		}
+
+		break;
+	case 3 :
+		for(i = 0; i < InLen; i++){
+			(*DO)[i*6]   = ((*DI)[i] & 0x1) ;
+			(*DO)[i*6+1] = ((*DI)[i] & 0x2) >> 1 ;
+			(*DO)[i*6+2] = ((*DI)[i] & 0x4) >> 2 ;
+			(*DO)[i*6+3] = ((*DI)[i] & 0x8) >> 3 ;
+			(*DO)[i*6+4] = ((*DI)[i] & 0x10) >> 4 ;
+			(*DO)[i*6+5] = ((*DI)[i] & 0x20) >> 5 ;
+			debug(V_DEBUG, "%d, %d \n",i,(*DO)[i]);
+		}
+		break;
+	default :
+		assert(Mod<=3);
+	}
+
+	debug(V_DEBUG,"Mux data out\n");
+
 }
