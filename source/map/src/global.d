1256a76544e1cb946f87de495afa948fb8747c5a
unknown <zhang.zhi@nationz.com.cn>
2017-10-23 18:09:10 +0800

    update map

---
+++ source/map/src/global.d
@@ -0,0 +1,8 @@
+src/global.o: src/global.c D:/msys32/mingw32/i686-w64-mingw32/include/math.h \
+ D:/msys32/mingw32/i686-w64-mingw32/include/crtdefs.h \
+ D:/msys32/mingw32/i686-w64-mingw32/include/_mingw.h \
+ D:/msys32/mingw32/i686-w64-mingw32/include/_mingw_mac.h \
+ D:/msys32/mingw32/i686-w64-mingw32/include/_mingw_secapi.h \
+ D:/msys32/mingw32/i686-w64-mingw32/include/vadefs.h \
+ D:/msys32/mingw32/i686-w64-mingw32/include/sdks/_mingw_directx.h \
+ D:/msys32/mingw32/i686-w64-mingw32/include/sdks/_mingw_ddk.h
